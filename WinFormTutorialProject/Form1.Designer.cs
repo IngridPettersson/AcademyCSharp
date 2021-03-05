4346880d2ab858a940465bfcf18747593f8bb9b8
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-03-05 11:11:18 +0100

    Add MVC

--- WinFormTutorialProject/Form1.Designer.cs
+++ WinFormTutorialProject/Form1.Designer.cs
@@ -29,13 +29,47 @@ namespace WinFormTutorialProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstNames = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Names";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lstNames
+            // 
+            this.lstNames.FormattingEnabled = true;
+            this.lstNames.ItemHeight = 16;
+            this.lstNames.Location = new System.Drawing.Point(12, 27);
+            this.lstNames.Name = "lstNames";
+            this.lstNames.Size = new System.Drawing.Size(120, 84);
+            this.lstNames.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(694, 411);
+            this.Controls.Add(this.lstNames);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Names";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstNames;
     }
 }
 
--- WinFormTutorialProject/Form1.cs
+++ WinFormTutorialProject/Form1.cs
@@ -16,5 +16,10 @@ namespace WinFormTutorialProject
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
