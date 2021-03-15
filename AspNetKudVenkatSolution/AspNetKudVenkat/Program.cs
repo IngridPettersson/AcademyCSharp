71c510d691abd677452c8edeba038b174bbd9420
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-03-15 09:31:44 +0100

    add changes

---
+++ AspNetKudVenkatSolution/AspNetKudVenkat/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetKudVenkat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Here is where the Console app is turned into a Web app.
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
