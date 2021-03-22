5321848c65b9ff34e78ed7564dd04c1f796f99a2
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-03-22 16:56:44 +0100

    Add changes

---
+++ WebAPISolution/WebAPI/Models/PhotosService.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebAPI.Models.ViewModels;
+
+namespace WebAPI.Models
+{
+    public class PhotosService
+    {
+        IHttpClientFactory httpClient;
+
+        public PhotosService(IHttpClientFactory httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<PhotosIndexVM[]> GetAllPhotos()
+        {
+            HttpClient client = httpClient.CreateClient();
+            string url = "https://jsonplaceholder.typicode.com/photos";
+            string json = await client.GetStringAsync(url);
+
+            Photo[] photoArr = JsonConvert.DeserializeObject<Photo[]>(json);
+
+            return photoArr
+                .Select(o => new PhotosIndexVM
+                {
+                    Title = o.Title,
+                    URL = o.Url
+                })
+                .ToArray();
+        }
+    }
+}
