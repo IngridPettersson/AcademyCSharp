64e44f50c8d7a75af2c60febde6137b530f5c22a
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-03-24 09:25:38 +0100

    Add ADO.NET solution

---
+++ ADO.NETHakanSolution/ADO.NETHakan/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ADO.NETHakan
+{
+    class Program
+    {
+        static SqlConnection sqlConnection = new SqlConnection();
+
+        static void Main(string[] args)
+        {
+            // ADO.NET är ett API som i sin enklaste form består av 3 klassser:
+            //1. SqlConnection
+            //2. SqlCommand
+            //      SqlParameter (behövs främst när vi ska anropa stored procedures)
+            //3. SqlDataReader
+            // För att använda ADO.NET behöver man endast kunna grundläggande SQL.
+
+        }
+    }
+}
