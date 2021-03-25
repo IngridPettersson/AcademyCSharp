f3a77241f54dedc30049de541a624e8c0d918d71
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-03-25 09:46:49 +0100

    Add Identity demo

---
+++ IdentityDemo_Start/IdentityDemo_Start/IdentityDemo/Controllers/AccountController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using IdentityDemo.Models;
+using IdentityDemo.Models.ViewModels;
+
+namespace IdentityDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        AccountService accountService;
+
+        public AccountController(AccountService accountService)
+        {
+            this.accountService = accountService;
+        }
+
+        [HttpGet]
+        [Route("members")]
+        public IActionResult Members()
+        {
+            return View(new AccountMembersVM { Username = User.Identity.Name });
+        }
+
+        [HttpGet]
+        [Route("")]
+        [Route("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Register(AccountRegisterVM viewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            // Try to register user
+            var success = accountService.TryRegister(viewModel);
+            if (!success)
+            {
+                // Show error
+                ModelState.AddModelError(string.Empty, "Failed to create user");
+                return View(viewModel);
+            }
+
+            // Redirect user
+            return RedirectToAction(nameof(Login));
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new AccountLoginVM { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(AccountLoginVM viewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            // Check if credentials is valid (and set auth cookie)
+            var success = accountService.TryLoginAsync(viewModel);
+            if (!success)
+            {
+                // Show error
+                ModelState.AddModelError(nameof(AccountLoginVM.Username), "Login failed");
+                return View(viewModel);
+            }
+
+            // Redirect user
+            if (string.IsNullOrWhiteSpace(viewModel.ReturnUrl))
+                return RedirectToAction(nameof(Members));
+            else
+                return Redirect(viewModel.ReturnUrl);
+        }
+    }
+}
---
+++ IdentityDemo_Start/IdentityDemo_Start/IdentityDemo/Models/AccountService.cs
@@ -0,0 +1,24 @@
+﻿using IdentityDemo.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityDemo.Models
+{
+    public class AccountService
+    {
+        public bool TryRegister(AccountRegisterVM viewModel)
+        {
+            // Todo: Try to create a new user
+            return false;
+        }
+
+        public bool TryLoginAsync(AccountLoginVM viewModel)
+        {
+            // Todo: Try to sign user
+            return true;
+        }
+    }
+}
